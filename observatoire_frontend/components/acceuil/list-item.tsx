750c78ceaffab1adf967d5dca49596e11278fefc
omarMOURID <mourid463@gmail.com>
2024-05-22 20:00:29 +0200

    modifier le design de l'acceuil, ajout du button pour créer un nouveau formulaire, ajout des titres pour avoir une description des boutons

--- observatoire_frontend/components/acceuil/list-item.tsx
+++ observatoire_frontend/components/acceuil/list-item.tsx
@@ -10,9 +10,16 @@ interface ListItemProps {
   }
 }
 
+/**
+ * Composant représentant un élément de liste affichant des informations sur un poste et permettant
+ * d'effectuer des actions comme modifier, envoyer, supprimer ou télécharger un formulaire.
+ *
+ * @param {ListItemProps} props - Les propriétés de l'élément de liste, y compris l'objet item contenant les détails du poste.
+ * @returns {JSX.Element} Un élément de liste avec des boutons d'action.
+ */
 const ListItem: FC<ListItemProps> = ({ item }) => (
-  <div className="rounded-lg px-4 py-2 mt-3 flex justify-between items-center bg-[#F0EEF2]">
-    <div className="text-align-center">
+  <div className="rounded-lg px-4 py-2 mt-3 flex justify-between items-center w-full bg-[#F0EEF2]">
+    <div className="text-align-center w-full">
       <h1 className="text-lg text-black inline-block">{`${item.fonction} - ${item.entreprise}`}</h1>
       {item.submitted && (
         <span className="rounded-sm text-xs ml-4 bg-[#FC9C64] px-4">
@@ -22,18 +29,18 @@ const ListItem: FC<ListItemProps> = ({ item }) => (
     </div>
     <div className="flex">
       {item.submitted ? (
-        <button className="w-8 h-8 flex items-center justify-center bg-[#1F944E] rounded-full">
+        <button className="w-8 h-8 flex items-center justify-center bg-[#1F944E] rounded-full" title="Télécharger le formulaire">
           <ArrowDownTrayIcon className="size-4" />
         </button>
       ) : (
         <>
-          <button className="w-8 h-8 flex items-center justify-center bg-[#3E1F94] rounded-full">
+          <button className="w-8 h-8 flex items-center justify-center bg-[#3E1F94] rounded-full" title="Modifier le formulaire">
             <PencilIcon className="size-4" />
           </button>
-          <button className="w-8 h-8 ml-2 flex items-center justify-center bg-[#1F944E] rounded-full">
+          <button className="w-8 h-8 ml-2 flex items-center justify-center bg-[#1F944E] rounded-full" title="Envoyer le formulaire">
             <CheckIcon className="size-4" />
           </button>
-          <button className="w-8 h-8 ml-2 flex items-center justify-center bg-[#D71A1A] rounded-full">
+          <button className="w-8 h-8 ml-2 flex items-center justify-center bg-[#D71A1A] rounded-full" title="Supprimer le formulaire">
             <TrashIcon className="size-4" />
           </button>
         </>

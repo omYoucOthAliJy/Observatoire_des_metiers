520118b6d2a95cecdd1c8bc803b3b6abc1581b53
omarMOURID <mourid463@gmail.com>
2024-05-22 20:00:29 +0200

    modifier le design de l'acceuil, ajout du button pour créer un nouveau formulaire, ajout des titres pour avoir une description des boutons

--- observatoire_frontend/pages/acceuil.tsx
+++ observatoire_frontend/pages/acceuil.tsx
@@ -1,5 +1,7 @@
 import ListItem from '@/components/acceuil/list-item';
+import { Button } from '@/components/form';
 import { Header } from '@/components/profile';
+import { PlusIcon } from "@heroicons/react/16/solid";
 import React from 'react';
 
 const fakeData = [
@@ -35,14 +37,22 @@ const fakeData = [
   },
 ]
 
+/**
+ * Composant d'accueil affichant une liste d'éléments avec la possibilité d'ajouter un nouveau formulaire.
+ *
+ * @returns {JSX.Element} La page d'accueil avec la liste des éléments et un bouton pour ajouter un nouveau formulaire.
+ */
 function Acceuil() {
   return (
     <div className="min-h-screen bg-white pb-8">
       <Header user={{ name: "El hamri Othman", role: "user" }} />
-      <div className="lg:container lg:mx-auto mt-8">
+      <div className="lg:container flex flex-col gap-4 p-4 w-full justify-start items-center mx-auto">
         {fakeData.map(item => (
           <ListItem key={item.id} item={item} />
         ))}
+        <Button title='Ajouter un nouveau formulaire'>
+          <PlusIcon className='size-6'/>
+        </Button>
       </div>
     </div>
   )

195b8fa50cf7a1dedff3852acdf8e0c186994b7c
jybuissereth <jbuissereth@outlook.fr>
2024-05-24 01:17:38 +0200

    creation dto et de roles guard, creation du finalisation du filtre unique

--- observatoire_backend/src/user/dto/create-user-dto.ts
+++ observatoire_backend/src/user/dto/create-user-dto.ts
@@ -4,4 +4,27 @@ export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
   readonly email: string;
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly last_name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly birthday: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly formation: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly date_diplome: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly description: string;
 }
